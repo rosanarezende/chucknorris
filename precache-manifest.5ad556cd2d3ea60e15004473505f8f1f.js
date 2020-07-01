1a96db129c565644ccae40189e17378714334219
Rosana Rezende <contato@rosanarezende.com>
2020-06-30 21:10:51 -0300

    Updates

--- precache-manifest.5ad556cd2d3ea60e15004473505f8f1f.js
+++ precache-manifest.5ad556cd2d3ea60e15004473505f8f1f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "026f7436574cc33242f37ceeccf587b1",
+    "revision": "88c4db0a4b805273d3085b71fba98eb4",
     "url": "/chucknorris/index.html"
   },
   {
-    "revision": "e412aa905d524a53ea8c",
+    "revision": "153dc3b7117ca6eec513",
     "url": "/chucknorris/static/css/main.5c95ffc2.chunk.css"
   },
   {
-    "revision": "37365d7fa86f405dc1e1",
-    "url": "/chucknorris/static/js/2.b66d66f5.chunk.js"
+    "revision": "2af577b99f84c0ef0f20",
+    "url": "/chucknorris/static/js/2.855fd161.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/chucknorris/static/js/2.b66d66f5.chunk.js.LICENSE.txt"
+    "url": "/chucknorris/static/js/2.855fd161.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e412aa905d524a53ea8c",
-    "url": "/chucknorris/static/js/main.36882f96.chunk.js"
+    "revision": "153dc3b7117ca6eec513",
+    "url": "/chucknorris/static/js/main.304d8808.chunk.js"
   },
   {
     "revision": "0cb52c1e3101bc45dff9",
